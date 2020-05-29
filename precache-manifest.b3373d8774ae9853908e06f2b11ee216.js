cff19b96667861f8549fe70ac188fe74e86397ee
QuickSwitch <heneverlistened@gmail.com>
2020-05-29 02:56:50 -0500

    Updates

---
+++ precache-manifest.b3373d8774ae9853908e06f2b11ee216.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b91117400eb104d3d3a6246c54cb2df",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "60b76751f8fef2ead7e1",
+    "url": "/ToDoApp/static/css/main.e55a3d81.chunk.css"
+  },
+  {
+    "revision": "d0f0a1ecd4d502b3061c",
+    "url": "/ToDoApp/static/js/2.a662508b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoApp/static/js/2.a662508b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60b76751f8fef2ead7e1",
+    "url": "/ToDoApp/static/js/main.d88129b3.chunk.js"
+  },
+  {
+    "revision": "6a1fe9b16be76f410c92",
+    "url": "/ToDoApp/static/js/runtime-main.4f670d30.js"
+  }
+]);
